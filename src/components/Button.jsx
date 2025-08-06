a8bd547a9b2a4f5ceb64b403222c1dc3ee30c3bc
R3Nexe <nishhu24@gmail.com>
2025-08-06 20:35:04 +0530

    resource cleanup

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,34 +1,34 @@
 import { useState } from 'react'
-import { easeIn, motion, scale } from 'framer-motion'
+import { motion } from 'framer-motion'
 
 const Button = () => {
-  const [ isHover, setisHover ] = useState(false)
+  const [isHover, setisHover] = useState(false)
   return (
     <div className='flex justify-center pt-3'>
       <motion.div className='flex relative overflow-clip border-1 p-2 rounded-full '
         onMouseEnter={(e) => setisHover(true)}
         onMouseLeave={(e) => setisHover(false)}
         animate={{
-          scale: isHover?1.1:1
+          scale: isHover ? 1.1 : 1
         }}
-        transition={{ease:"easeIn"}}
+        transition={{ ease: "easeIn" }}
       >
         <motion.div className='absolute size-2 bg-bright-purple rounded-full'
-          initial={{opacity:0}}
+          initial={{ opacity: 0 }}
           animate={{
-            width: isHover? 40:0,
+            width: isHover ? 40 : 0,
             scale: isHover ? 40 : 1,
-            opacity: isHover?1:1,
+            opacity: isHover ? 1 : 1,
           }}
-          transition={{duration:.5,
-            ease:"easeInOut"
+          transition={{
+            duration: .5,
+            ease: "easeInOut"
           }}></motion.div>
         <motion.a
-        className='text-white z-5 '
-          href="public/resume/resume.pdf" // Make sure your PDF is in the `public` folder
+          className='text-white z-5 '
+          href="/resume/webdevResume.pdf"
           target="_blank"
           rel="noopener noreferrer"
-
         >
           View Resume
         </motion.a>
