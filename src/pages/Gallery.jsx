4789a6aef8b5df2a83ec7b002821612ae778ec08
R3Nexe <nishhu24@gmail.com>
2025-09-08 14:56:01 +0530

    added new project

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,10 +1,9 @@
-const Gallery = () => {
+import { useEffect, useState } from "react";
+
+export default function Gallery() {
   return (
-    <section className="w-full h-screen text-center items-center justify-center">
-      <div className="my-[45vh] mx-auto">
-        <h1 className="font-head text-gradient text-7xl">Work in progress</h1>
-      </div>
-    </section>
+    <div className="mt-[20vh] columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-4 p-10">
+      <div className="animate-pulse bg-white/10 rounded-lg w-full h-[200vh]" />
+    </div>
   );
-};
-export default Gallery;
+}
